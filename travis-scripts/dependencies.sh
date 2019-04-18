8be691576122b584c83f5d465f22d3c55e12008a
julien lopez <julien.lopez51@gmail.com>
2019-04-18 11:06:34 +0200

    trying to fix compilation on travis-ci

--- travis-scripts/dependencies.sh
+++ travis-scripts/dependencies.sh
@@ -3,6 +3,6 @@
 sudo add-apt-repository -y ppa:ubuntu-toolchain-r/test
 sudo apt-get update -qq
 sudo apt-get update
-sudo apt-get install -y cmake clang-6 clang++-6 lcov
+sudo apt-get install -y cmake clang-5 clang++-5 lcov
 
-sudo update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-6 90
+# sudo update-alternatives --install /usr/bin/gcov gcov /usr/bin/gcov-5 90
